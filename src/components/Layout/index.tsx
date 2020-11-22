0dc8f56e01ce907133c09e73e7fc06267057639c
ayrbox <sabin.dangol@hotmail.com>
2020-11-22 16:04:47 +0000

    Refactor styles.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,52 @@
 import * as React from 'react';
-import '../../css/resetCSS';
-import '../../css/siteCSS';
+import styled from '@emotion/styled';
 
-import { LayoutWrapper } from './layout.styles';
+import '@styles/ResetCss';
+import '@styles/GlobalCss';
+
+const LayoutWrapper = styled.div`
+  overflow: hidden;
+  .nav-wrapper {
+    border: 2px solid red;
+    background: navy;
+    color: yellow;
+
+    .main-menu {
+      margin-right: 40px;
+      padding: 0;
+
+      li {
+        display: inline-block;
+        padding-left: 16px;
+        padding-right: 16px;
+        &:first-of-type {
+          padding-left: 0;
+        }
+        &:last-child {
+          padding-right: 0;
+        }
+        &.is-current {
+          a {
+            color: green;
+          }
+        }
+        a {
+          padding: 5px;
+          font-size: 16px;
+          font-weight: 400;
+          color: #fff;
+          transition: 0.15s ease-in-out;
+          &:hover {
+            color: #edca37;
+          }
+        }
+        @media (max-width: 990px) {
+          display: none;
+        }
+      }
+    }
+  }
+`;
 
 const Layout = ({ children }) => {
   return <LayoutWrapper>{children}</LayoutWrapper>;
