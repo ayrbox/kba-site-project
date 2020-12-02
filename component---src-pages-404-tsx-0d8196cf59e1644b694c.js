953fb90c781290ebaaaf51ff083979e66138d8f7
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2020-12-02 23:05:27 +0000

    Updates

---
+++ component---src-pages-404-tsx-0d8196cf59e1644b694c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),u=e.n(o);t.default=function(){return u.a.createElement("h1",null,"404 | Not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0d8196cf59e1644b694c.js.map
